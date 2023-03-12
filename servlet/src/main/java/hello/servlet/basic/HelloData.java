fee329a1e1c76d418cf05c1f1e38343fb2bd7052
bjw5035 <send2ugfd@gmail.com>
2023-03-12 16:10:57 +0900

    test

---
+++ servlet/src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,13 @@
+package hello.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter // 알아서 getter,setter을 넣어줌
+public class HelloData {
+
+	private String username;
+	private int age;
+
+	
+}
